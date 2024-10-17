a63c8fe51f9416512e7f5209959c451fbc946913
mfaux <mf@configit.com>
2024-10-17 22:52:24 +0200

    feat: updates to the project card

--- components/ui/submit-button.tsx
+++ components/ui/submit-button.tsx
@@ -4,7 +4,6 @@ import { ArrowUp } from 'lucide-react';
 import { Button, ButtonProps } from './button';
 
 const SubmitButton = (props: ButtonProps & { isPending: boolean }) => {
-  console.log('SubmitButton');
   return (
     <Button type="submit" aria-disabled={props.isPending} {...props}>
       <ArrowUp />
